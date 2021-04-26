7a3eb2482977ddb6e0212da57adb53e0b9fc3d4c
desfar <desfarik@gmail.com>
2021-04-26 23:44:54 +0300

    test linters

---
+++ src/test.js
@@ -0,0 +1,16 @@
+const s = 23;
+
+let v;
+if (s === 2) {
+    v = getRule();
+}
+
+function getRule() {
+    return [12].forEach(e => {
+        return e;
+    });
+}
+
+const b = 'b2';
+const a = `123132${b}dsfsd`;
+console.log(a + b + v);
